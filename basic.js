18878df1812b1f97b1cc03debfafeabb11219593
anand-2020 <62353883+anand-2020@users.noreply.github.com>
2022-06-07 10:23:10 +0530

    simple socket connection

---
+++ basic.js
@@ -0,0 +1,28 @@
+// We need http because we dont have express
+const http = require("http");
+// We need socketio...it's 3rd party!
+const socketio = require("socket.io");
+
+// We make an http server with node!
+const server = http.createServer((req, res) => {
+  res.end("I am connected!");
+});
+
+const io = socketio(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket, req) => {
+  // ws.send because socket.emit
+  socket.emit("welcome", "Welcome to the websocket server!!");
+  // no change here...
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
+});
+
+server.listen(4000);
